af301bdbcf281701768b94da81472a4d3d681cd1
Luke Francis <lukefrancis@Rhondas-MacBook-Pro.local>
2015-07-26 20:59:20 -0400

    Initial setup

---
+++ test/Secret.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.*;
+
+public class Secret
+{
+    public Secret()
+    {
+        // modify calc somehow?
+    }
+    
+    public Integer calc(Integer input)
+    {
+        return input;
+    }
+}
---
+++ test/TestSecret.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.*;
+
+public class TestSecret
+{
+    public TestSecret()
+    {
+    }
+    
+    public static void main(String args[])
+    {
+        Secret secret = new Secret();
+        System.out.println("Hello World");
+        
+    }
+    
+}
